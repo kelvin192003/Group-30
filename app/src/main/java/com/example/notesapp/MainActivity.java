b0351588c5913e5f808721cbfbae82552a858177
JosephAwuku33 <33jaykay@gmail.com>
2023-07-07 08:39:54 +0000

    Adding the Premium Page

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -13,6 +14,7 @@ import com.google.android.material.button.MaterialButton;
 public class MainActivity extends AppCompatActivity {
 
     MaterialButton matButton;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent;
                 intent = new Intent(MainActivity.this, NoteList.class);
+                startActivity(intent);
+            }
+        });
+
+        imageView = findViewById(R.id.imagePic);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(MainActivity.this, PremiumPage.class);
                 startActivity(intent);
             }
         });
